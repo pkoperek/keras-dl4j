ab526278d56aa06c0e00463bc9f787904176bbb3
Pawel <pkoperek@gmail.com>
2017-01-13 22:59:30 +0000

    package definition

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='kerasdl4j',
+    version='0.1',
+    description='Use Deeplearning4j as backend for Keras',
+    author='Pawel Koperek',
+    author_email='pkoperek@gmail.com',
+    url='https://github.com/pkoperek/keras-dl4j',
+    packages=['kerasdl4j'],
+    requires=['keras', 'py4j', 'h5py', 'xxhash'],
+)
